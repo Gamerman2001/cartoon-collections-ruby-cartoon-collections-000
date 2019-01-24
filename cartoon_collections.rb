483ef27c927ff9267c87563bb55c71fd2a2ba51f
Troy Gooden <tro12185@gmail.com>
2019-01-24 19:28:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,10 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(arr)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = ["cheddar", "gouda", "camembert", ]
   arr.find {|x| cheese_types.include?(x)}
+end
+
+def words_with_b(arr)
+  
 end
